ec9e57b879aca074b13440da1c752b708f39ff39
Christian Schwarz <me@cschwarz.com>
2018-06-27 16:59:39 +0200

    Some microbenchmarks and a baseline that just uses TCP.
    On loopback on separate cores (no hyperthreading), I get around
    34000Mbit/s with baseline and 31000Mbit/s with streamenc.

---
+++ microbenchmarks/streamenc/baseline/main.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	flag "github.com/spf13/pflag"
+	"log"
+
+	"regexp"
+	"net"
+	"io"
+	"strconv"
+	"os"
+	"time"
+	"encoding/binary"
+)
+
+func main() {
+
+	var mode string
+	var bufsiz int
+
+	flag.StringVar(&mode, "mode", "client|server", "")
+	flag.IntVar(&bufsiz, "c.bufsiz", 1 << 21, "")
+	flag.Parse()
+
+	buf := make([]byte, bufsiz)
+
+	serverRE := regexp.MustCompile(`^server:(tcp):(.*:.*)$`)
+	clientRE := regexp.MustCompile(`^client:(tcp):(.*:.*):(.*)$`)
+	switch {
+	case clientRE.MatchString(mode):
+
+		m := clientRE.FindStringSubmatch(mode)
+		nbytes, err := strconv.ParseInt(m[3], 0, 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("begin Dial")
+		conn, err := net.Dial(m[1], m[2])
+		if err != nil {
+			log.Fatal(err)
+		}
+		func () {
+			defer conn.Close()
+			log.Printf("begin request")
+			begin := time.Now()
+
+			if err := binary.Write(conn, binary.BigEndian, nbytes); err != nil {
+				log.Fatal(err)
+			}
+
+			var n int64
+			for n < nbytes {
+				readc := int64(len(buf))
+				if nbytes - n < readc {
+					readc = nbytes - n
+				}
+
+				n1, err := conn.Read(buf[0:readc])
+				if n1 > 0 {
+					n += int64(n1)
+					n2, err := os.Stdout.Write(buf[0:n1])
+					if err != nil || n2 != n1 {
+						log.Fatal(err)
+					}
+				}
+				if err != nil {
+					if err != io.EOF {
+						log.Fatal(err)
+					}
+					break
+				}
+			}
+
+			delta := time.Now().Sub(begin)
+			byteps := float64(n) / delta.Seconds()
+			log.Printf("transferred %d bytes in %s, this amounts to %v MB/s or %v Mb/s", n, delta, byteps/1e6, (byteps/1e6)*8)
+			if err != nil {
+				log.Fatal(err)
+			}
+			if n != nbytes {
+				log.Fatalf("n = %v != %v = nbytes", n, nbytes)
+			}
+			log.Printf("request done")
+		}()
+
+	case serverRE.MatchString(mode):
+		m := serverRE.FindStringSubmatch(mode)
+		l, err := net.Listen(m[1], m[2])
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer l.Close()
+
+		for {
+			conn, err := l.Accept()
+			if err != nil {
+				log.Fatal(err)
+			}
+			func() {
+				defer conn.Close()
+				log.Printf("begin ServeConn: %s", conn)
+				defer log.Printf("finished ServeConn")
+
+
+				var nbytes int64
+				if err := binary.Read(conn, binary.BigEndian, &nbytes); err != nil {
+					log.Fatal(err)
+				}
+				z, err := os.Open("/dev/zero")
+				if err != nil {
+					log.Fatal(err)
+				}
+				defer z.Close()
+
+				r := io.LimitReader(z, nbytes)
+				for {
+					readc := int64(len(buf))
+					n1, err := r.Read(buf[0:readc])
+					if n1 > 0 {
+						n2, err := conn.Write(buf[0:n1])
+						if err != nil || n2 != n1 {
+							log.Fatal(err)
+						}
+					}
+					if err != nil {
+						if err != io.EOF {
+							log.Fatal(err)
+						}
+						break
+					}
+				}
+			}()
+		}
+
+
+	default:
+		log.Fatal("specify -mode client|server")
+	}
+
+}
---
+++ microbenchmarks/streamenc/main.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	flag "github.com/spf13/pflag"
+	"log"
+
+	"regexp"
+	"net"
+	"github.com/problame/go-streamrpc"
+	"bytes"
+	"io"
+	"strconv"
+	"os"
+	"context"
+	"fmt"
+	"time"
+)
+
+func handleDevZeroStream(endpoint string, reqStructured *bytes.Buffer, reqStream io.Reader) (resStructured *bytes.Buffer, resStream io.Reader, err error) {
+	streamLen, err := strconv.ParseInt(reqStructured.String(), 0, 64)
+	if err != nil {
+		return nil, nil, err
+	}
+	z, err := os.Open("/dev/zero")
+	if err != nil {
+		return nil, nil, err
+	}
+	return bytes.NewBufferString("OK"), io.LimitReader(z, streamLen), nil
+}
+
+func main() {
+
+	var mode string
+	connConfig := streamrpc.ConnConfig{
+		RxStreamMaxChunkSize: 0,
+		RxHeaderMaxLen: 1024,
+		RxStructuredMaxLen: 1 << 16,
+		TxChunkSize: 0,
+	}
+
+	flag.StringVar(&mode, "mode", "client|server", "")
+	flag.Uint32Var(&connConfig.TxChunkSize, "c.txcsiz", 1 << 21 , "")
+	flag.Uint32Var(&connConfig.RxStreamMaxChunkSize, "c.rxmaxcsiz", 1 << 21, "")
+	flag.Parse()
+
+	serverRE := regexp.MustCompile(`^server:(tcp):(.*:.*)$`)
+	clientRE := regexp.MustCompile(`^client:(tcp):(.*:.*):(.*)$`)
+	switch {
+	case clientRE.MatchString(mode):
+
+		m := clientRE.FindStringSubmatch(mode)
+		nbytes, err := strconv.ParseInt(m[3], 0, 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("begin Dial")
+		conn, err := net.Dial(m[1], m[2])
+		if err != nil {
+			log.Fatal(err)
+		}
+		func () {
+			defer conn.Close()
+			log.Printf("begin request")
+			begin := time.Now()
+
+			client, err := streamrpc.NewClientOnConn(conn, &connConfig)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer client.Close()
+
+			rstru, rstre, err := client.RequestReply(context.Background(), "foo", bytes.NewBufferString(fmt.Sprintf("%v", nbytes)), nil)
+			if err != nil {
+				log.Fatal(err)
+			}
+			if rstru.String() != "OK" {
+				log.Fatalf("unexpected response: %#v", rstru)
+			}
+
+			n, err := io.Copy(os.Stdout, rstre)
+			delta := time.Now().Sub(begin)
+			byteps := float64(n) / delta.Seconds()
+			log.Printf("transferred %d bytes in %s, this amounts to %v MB/s or %v Mb/s", n, delta, byteps/1e6, (byteps/1e6)*8)
+			if err != nil {
+				log.Fatal(err)
+			}
+			if n != nbytes {
+				log.Fatalf("n = %v != %v = nbytes", n, nbytes)
+			}
+			log.Printf("request done")
+		}()
+
+	case serverRE.MatchString(mode):
+		m := serverRE.FindStringSubmatch(mode)
+		l, err := net.Listen(m[1], m[2])
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer l.Close()
+
+		for {
+			conn, err := l.Accept()
+			if err != nil {
+				log.Fatal(err)
+			}
+			func() {
+				defer conn.Close()
+				log.Printf("begin ServeConn: %s", conn)
+				if err := streamrpc.ServeConn(conn, &connConfig, handleDevZeroStream); err != nil {
+					log.Printf("error ServeConn: %s", err)
+				} else {
+					log.Printf("finished ServeConn")
+				}
+			}()
+		}
+
+
+	default:
+		log.Fatal("specify -mode client|server")
+	}
+
+}
